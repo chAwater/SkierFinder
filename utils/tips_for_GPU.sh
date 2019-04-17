651155b2d3e30eb9635322159b277c359015c8fe
chenhe <38067903+chAwater@users.noreply.github.com>
2019-04-17 13:49:20 +0800

    Add Tips for GPU

--- utils/tips_for_GPU.sh
+++ utils/tips_for_GPU.sh
@@ -10,3 +10,6 @@
 # https://github.com/matterport/Mask_RCNN/issues/958
 # > AttributeError: 'ParallelModel' object has no attribute '_is_graph_network'
 conda install keras=2.1.3 tensorflow-gpu
+
+# https://github.com/matterport/Mask_RCNN/pull/1082
+sed -i'' 's!\[self.config.BATCH_SIZE![self.config.IMAGES_PER_GPU!' ./Mask_RCNN/mrcnn/model.py
